ef82fc3ff785a6bb6476b2dadfc50939124e8f24
Aaron Mavrinac <mavrinac@gmail.com>
2010-11-27 14:14:46 -0500

    Add the rest of the basic 2-input gates.

--- dilo/devices/gates.py
+++ dilo/devices/gates.py
@@ -80,3 +80,93 @@ class ORGate(Device):
             self._outputs['q'] = True
         else:
             self._outputs['q'] = False
+
+
+class NANDGate(Device):
+    """\
+    NAND gate class.
+    """
+    def __init__(self):
+        """\
+        Constructor.
+        """
+        super(NANDGate, self).__init__()
+        self._inputs = {'a': False, 'b': False}
+        self._outputs = {'q': False}
+
+    def _update(self):
+        """\
+        Update outputs based on inputs.
+        """
+        if self._inputs['a'] and self._inputs['b']:
+            self._outputs['q'] = False
+        else:
+            self._outputs['q'] = True
+
+
+class NORGate(Device):
+    """\
+    NOR gate class.
+    """
+    def __init__(self):
+        """\
+        Constructor.
+        """
+        super(NORGate, self).__init__()
+        self._inputs = {'a': False, 'b': False}
+        self._outputs = {'q': False}
+
+    def _update(self):
+        """\
+        Update outputs based on inputs.
+        """
+        if self._inputs['a'] or self._inputs['b']:
+            self._outputs['q'] = False
+        else:
+            self._outputs['q'] = True
+
+
+class XORGate(Device):
+    """\
+    XOR gate class.
+    """
+    def __init__(self):
+        """\
+        Constructor.
+        """
+        super(XORGate, self).__init__()
+        self._inputs = {'a': False, 'b': False}
+        self._outputs = {'q': False}
+
+    def _update(self):
+        """\
+        Update outputs based on inputs.
+        """
+        if self._inputs['a'] or self._inputs['b'] \
+        and not (self._inputs['a'] and self._inputs['b']):
+            self._outputs['q'] = True
+        else:
+            self._outputs['q'] = False
+
+
+class XNORGate(Device):
+    """\
+    XNOR gate class.
+    """
+    def __init__(self):
+        """\
+        Constructor.
+        """
+        super(XNORGate, self).__init__()
+        self._inputs = {'a': False, 'b': False}
+        self._outputs = {'q': False}
+
+    def _update(self):
+        """\
+        Update outputs based on inputs.
+        """
+        if self._inputs['a'] or self._inputs['b'] \
+        and not (self._inputs['a'] and self._inputs['b']):
+            self._outputs['q'] = False
+        else:
+            self._outputs['q'] = True

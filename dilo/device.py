9574974dadb22748bd94df602499ad2679669944
Aaron Mavrinac <mavrinac@gmail.com>
2010-12-01 15:08:22 -0500

    Add Sender and Receiver. Fill in some docstrings.

--- dilo/device.py
+++ dilo/device.py
@@ -89,8 +89,14 @@ class Logic0(Device):
     Logic zero.
     """
     def __init__(self):
+        """\
+        Constructor.
+        """
         super(Logic0, self).__init__()
         self._outputs = {'q': False}
+
+    def _update(self):
+        pass
 
 
 class Logic1(Device):
@@ -98,8 +104,17 @@ class Logic1(Device):
     Logic one.
     """
     def __init__(self):
+        """\
+        Constructor.
+        """
         super(Logic1, self).__init__()
         self._outputs = {'q': True}
+
+    def _update(self):
+        """\
+        Update outputs based on inputs.
+        """
+        pass
 
 
 class Buffer(Device):
@@ -107,12 +122,91 @@ class Buffer(Device):
     Buffer/node device class.
     """
     def __init__(self):
+        """\
+        Constructor.
+        """
         super(Buffer, self).__init__()
         self._inputs = {'a': False}
         self._outputs = {'q': True}
 
     def _update(self):
+        """\
+        Update outputs based on inputs.
+        """
         self._outputs['q'] = self._inputs['a']
+
+
+class Sender(Buffer):
+    """\
+    Sender class.
+    """
+    outputs = {}
+
+    def __init__(self):
+        """\
+        Constructor.
+        """
+        super(Sender, self).__init__()
+        self._receiver = None
+
+    @property
+    def receiver(self):
+        """\
+        The associated receiver.
+        """
+        return self._receiver
+
+    @receiver.setter
+    def receiver(self, value):
+        """\
+        Set the associated receiver.
+        """
+        if not isinstance(value, Receiver):
+            raise TypeError('invalid receiver')
+        if self._receiver:
+            self._receiver._sender = None
+        self._receiver = value
+        self._receiver._sender = self
+
+    @receiver.deleter
+    def receiver(self):
+        """\
+        Unset the associated receiver.
+        """
+        if self._receiver:
+            self._receiver._sender = None
+            self._receiver = None
+
+    def _update(self):
+        """\
+        Update outputs based on inputs. Also update the associated receiver if
+        it has been set.
+        """
+        super(Sender, self)._update()
+        if self._receiver:
+            self._receiver._inputs['a'] = self._outputs['q']
+            self._receiver._update()
+
+
+class Receiver(Buffer):
+    """\
+    Receiver class.
+    """
+    inputs = {}
+
+    def __init__(self):
+        """\
+        Constructor.
+        """
+        super(Receiver, self).__init__()
+        self._sender = None
+
+    @property
+    def sender(self):
+        """\
+        The associated sender.
+        """
+        return self._sender
 
 
 class Circuit(Device):
@@ -127,12 +221,18 @@ class Circuit(Device):
         self._cached_outputs = {}
 
     def _internal_inputs(self):
+        """\
+        Return all unconnected inputs of the circuit's devices.
+        """
         return ['%s.%s' % (deviceid, inputid) \
             for deviceid in self._devices.keys() \
             for inputid in self._devices[deviceid].inputs \
             if not self._connections.has_key((deviceid, inputid))]
     
     def _internal_outputs(self):
+        """\
+        Return all outputs of the circuit's devices.
+        """
         return ['%s.%s' % (deviceid, outputid) \
             for deviceid in self._devices.keys() \
             for outputid in self._devices[deviceid].outputs]
